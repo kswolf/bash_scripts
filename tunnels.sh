e5a4e9a673a6aea41be114457552cbb14c9f8906
Himanshu Dewangan <hd@192.168.1.5>
2022-09-13 19:02:00 +0530

    local

--- tunnels.sh
+++ tunnels.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/github -N -f -L localhost:8888:localhost:8888 ec2-user@hdewangan.dev2.eightfold.ai
+ssh -i ~/.ssh/id_rsa -N -f -L localhost:8888:localhost:8888 ec2-user@hdewangan.dev2.eightfold.ai
