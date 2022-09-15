86c44c407846f5738e241981db9d30a4e5106c0d
Himanshu Dewangan <hd@192.168.1.5>
2022-09-15 10:47:40 +0530

    fix

--- tunnels.sh
+++ tunnels.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa -N -f -L localhost:8888:localhost:8888 ec2-user@hdewangan.dev2.eightfold.ai
+ssh -i ~/.ssh/id_rsa -N -f -L localhost:8888:localhost:8888 ec2-user@172.31.20.207 -v
